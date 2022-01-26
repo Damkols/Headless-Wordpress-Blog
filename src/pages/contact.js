94f61d3649f79858c62f3c35e0f9e8e04810001b
damkols <damolausmankolapo@gmail.com>
2022-01-26 15:03:35 -0800

    built and styled contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,65 @@
 import React from "react";
 import Layout from "../Layouts/Layout";
+import { useState } from "react";
+import "../styles/contact.scss";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <Layout>
-      <div>
-        <h1>Contact Form</h1>
+      <div className="container">
+        <div className="contact">
+          <form onSubmit={handleSubmit}>
+            <div className="input">
+              <label>Your Name</label>
+              <input
+                type="text"
+                value={name}
+                name="name"
+                required
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+            </div>
+
+            <div className="input">
+              <label>Your Email</label>
+              <input
+                type="text"
+                value={email}
+                name="email"
+                required
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </div>
+
+            <div className="message">
+              <label>Your Message</label>
+              <textarea
+                required
+                value={message}
+                name="message"
+                onChange={(e) => {
+                  setMessage(e.target.value);
+                }}
+              ></textarea>
+            </div>
+
+            <div className="input">
+              <input type="submit" name="Submit" className="submit" />
+            </div>
+          </form>
+        </div>
       </div>
     </Layout>
   );
