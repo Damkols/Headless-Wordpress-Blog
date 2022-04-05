05eee518a40a477e75f5f8e3fd3d227198d44769
damkols <damolausmankolapo@gmail.com>
2022-04-05 11:47:49 -0800

    fixed some styling issues

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,37 +10,10 @@ const About = () => {
         <div className="me">
           <h2>Who am i ?</h2>
           <p>
-            I'm <b>Damola</b> , a frontend developer.
-            <br />
-            My stacks of development are ReactJS, Gatsby, GraphQL, HTML, CSS,
-            SCSS, JavaScript, I love React and SCSS for frontend development
+            I'm <b>Damola</b>, a frontend developer.
           </p>
-        </div>
 
-        <div className="lovedWorks">
-          <h2>My Loved Works</h2>
-          <p>
-            I love algorithms and data structure due to my passion for
-            mathematics. I do take challenges in some sites, so I prefer
-            challenges with time and gives solution result in percentage due to
-            the time complexity and test passed by the code.
-          </p>
-        </div>
-
-        <div className="targets">
-          <h2>Target</h2>
-          <p>
-            I'm working to be a great developer in future, still code everyday..
-          </p>
-        </div>
-
-        <div className="partingWords">
-          <h2>Word For Programmers</h2>
-          <p>
-            Coding all night doesn't make you better than other, but focusing
-            and practicing does . Try to follow a scheme of practice and
-            challenge
-          </p>
+          <p></p>
         </div>
       </div>
     </Layout>
